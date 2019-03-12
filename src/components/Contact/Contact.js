a625ec52bdbb17ee752d959c5f2828255932b57d
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-03-12 15:21:22 -0400

    add smooth scrolling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div className='contact' id='contact'>
-            <h2 className='contact-heading'>Contact Me</h2>
+            <h2 className='contact-heading'>Contact</h2>
             <a href='mailto:sheppard.charlie24@gmail.com' className='icon-link'><i class="far fa-envelope fa-3x"></i></a>
             <a href='tel:8022384089' className='icon-link'><i class="fas fa-phone fa-3x"></i></a>
             <a href='https://github.com/charliepsheppard' target = "_blank" className='icon-link'><i className="fab fa-github fa-3x"></i></a>
