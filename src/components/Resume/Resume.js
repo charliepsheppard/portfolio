e7b14071392c94e7a6edfbaa3deb9bcb37b0a8d5
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-03-12 13:40:49 -0400

    updated broken resume pdf link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Resume.css'
-import PDF from '../../assets/devResume.pdf'
 
 const Resume = () => {
     return (
         <div>
             <h2 className="resume-heading" id="resume">Resume</h2>
             <p>For the sake of scrolling, feel free to check out my resume below...</p>
-            <a href = {PDF} target = "_blank"><button className="resume-button">View Resume</button></a>
+            <a href = 'https://resume.creddle.io/resume/3chw68y0606' target = "_blank"><button className="resume-button">View Resume</button></a>
         </div>
     )
 }
