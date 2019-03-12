01b94f0e7cb718db314f509bdbc743a778b725b2
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-03-12 13:33:26 -0400

    fully responsive design, edited package.json and removed excess

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react'
 import './Header.css'
 // import { faLinkedin } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,7 +11,7 @@ const Header = () =>  {
                     <a href="#about"><li className='nav-link'>About</li></a>
                     <a href="#work"><li className='nav-link'>Work</li></a>
                     <a href="#resume"><li className='nav-link'>Resume</li></a>
-                    <a href="#contact"><li className='nav-link'>Contact Me</li></a>
+                    <a href="#contact"><li className='nav-link'>Contact</li></a>
                     <a href="https://github.com/charliepsheppard" target = "_blank"><i className="fab fa-github fa-2x"></i></a>
                     <a href="https://www.linkedin.com/in/charlespsheppard/" target = "_blank"><i className="fab fa-linkedin-in fa-2x"></i></a>
                 </ul>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import photo from '../../assets/myPhoto.png'
 const Home = () => {
     return (
         <div id='home-page'>
-            <img src={photo} className="photo" />
+            <img src={photo} className="photo" alt='headshot' />
             <h1 className='home-heading'><span className='hi'>Hi, </span><br /><span className='charlie'>I'm Charlie</span></h1>
         </div>
     )
